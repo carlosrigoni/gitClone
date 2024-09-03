4bdb3e26565f497254bef9f1563136a806c9477a
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2024-09-02 21:54:52 -0300

    feat: Add write, isUpToDate, getHash, and createFromCurrentState methods to Commit class

--- domain/src/main/Commit.js
+++ domain/src/main/Commit.js
@@ -14,4 +14,32 @@ class Commit {
     content += `Date: ${this.date.toUTCString()}\n\n${this.message}\n`;
     return content;
   }
+
+  write(objectsRepository) {
+    const content = this.generateCommitContent();
+    const commitHash = objectsRepository.write(content);
+    return commitHash;
+  }
+
+  isUpToDate(otherCommitHash, objectsRepository) {
+    const otherCommitAncestors =
+      objectsRepository.getAncestors(otherCommitHash);
+    return otherCommitAncestors.includes(this.getHash(objectsRepository));
+  }
+
+  getHash(objectsRepository) {
+    const content = this.generateCommitContent();
+    return objectsRepository.hash(content);
+  }
+
+  static createFromCurrentState(index, message, refs, objectsRepository) {
+    const treeHash = index.writeTree();
+    const parentHashes = refs.getParentHashes();
+    const commit = new Commit(treeHash, message, parentHashes);
+    const commitHash = commit.write(objectsRepository);
+    refs.updateHead(commitHash);
+    return commit;
+  }
 }
+
+export default Commit;
